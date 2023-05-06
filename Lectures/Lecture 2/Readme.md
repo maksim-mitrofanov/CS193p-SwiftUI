77b3acd9e8d7302308f825922575f1d9858f450d
maksim_mitrofanov <keyseymiroff@gmail.com>
2023-05-06 21:09:39 +0400

    Lecture 2

---
+++ Lectures/Lecture 2/Readme.md
@@ -0,0 +1,10 @@
+#  CS193p-SwiftUI-Lecture 2
+
+## Lecture Topic
+Introduction to Xcode, Swift, SwiftUI
+
+## Objective 
+Create a rounded rectangle with a text in the center
+
+## Demo
+<img src="https://user-images.githubusercontent.com/87092187/236478702-c67efd86-dcfb-4d47-bbf5-48760552f359.png"  width="30%">
--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -8,25 +8,95 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis = ["🐶", "🐱", "🐭", "🐹", "🐰", "🦊", "🐻", "🐼", "🐻‍❄️", "🐨", "🐯", "🦁", "🐮", "🐷", "🐸", "🐵"]
+    let columns = [GridItem(.adaptive(minimum: 80))]
+    @State var shownEmojiCount = 4
+    
     var body: some View {
-        VStack(spacing: 0){
-            ZStack {
-                RoundedRectangle(cornerRadius: 25)
-                    .foregroundColor(.white)
-                
-                RoundedRectangle(cornerRadius: 25)
-                    .strokeBorder(lineWidth: 4)
-                    .foregroundColor(.orange)
-                
-                VStack {
-                    Text("Memorize 👋")
-                        .font(.largeTitle)
+        VStack {
+            Text("Memorize!")
+                .font(.system(size: 30, design: .serif))
+                .padding(.top)
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 10){
+                    ForEach(emojis[0..<shownEmojiCount], id: \.self) { emoji in
+                        CardView(title: emoji, fillColor: .orange)
+                    }
                 }
-                
+                .padding()
             }
-            .padding(.horizontal, 30)
-            .padding(.vertical, 120)
-            .background(Color.black)
+            Spacer()
+            bottomButtons
+        }
+    }
+    
+    var bottomButtons: some View {
+        HStack {
+            removeCardsButton
+            addCardsButton
+        }
+        .buttonStyle(.bordered)
+        .font(.title2)
+        .padding()
+    }
+    
+    var addCardsButton: some View {
+        Button {
+            if shownEmojiCount < emojis.count { shownEmojiCount += 1}
+        } label: {
+            Image(systemName: "plus.circle")
+        }
+    }
+    
+    var removeCardsButton: some View {
+        Button {
+            if shownEmojiCount > 2 { shownEmojiCount -= 1}
+        } label: {
+            Image(systemName: "minus.circle")
+        }
+    }
+}
+
+struct CardView: View {
+    //Params
+    let title: String
+    var cornerRadius: CGFloat = 16
+    var borderWidth: CGFloat = 4
+    var fillColor: Color = .orange
+    let aspectRatio: CGFloat = 2/3
+    
+    //State
+    @State var isFaceUp: Bool = true
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp { faceUpView }
+            else { faceDownView }
+        }
+        .aspectRatio(aspectRatio, contentMode: .fit)
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
+    
+    var faceUpView: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: cornerRadius)
+                .foregroundColor(.white)
+            
+            RoundedRectangle(cornerRadius: cornerRadius)
+                .strokeBorder(lineWidth: borderWidth)
+                .foregroundColor(fillColor)
+            
+            Text(title)
+                .font(.largeTitle)
+        }
+    }
+    
+    var faceDownView: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: cornerRadius)
+                .foregroundColor(fillColor)
         }
     }
 }
