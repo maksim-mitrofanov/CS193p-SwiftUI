1007366d5bfecdc4633da09fdc0b243556191d15
Maksim Mitrofanov <keyseymiroff@gmail.com>
2023-05-19 12:41:10 +0400

    Created SET Project

---
+++ Assignments/Assignment 3/SET Game/SET Game/SET_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SET_GameApp.swift
+//  SET Game
+//
+//  Created by Максим Митрофанов on 19.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SET_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
