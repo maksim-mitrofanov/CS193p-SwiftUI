9fedc4da4ef1fd8bf647fc5690c2a6db13bbfc52
Maksim Mitrofanov <keyseymiroff@gmail.com>
2023-05-22 13:27:33 +0400

    Added Cardify ViewModifier

---
+++ Memorize/Memorize/View Modifiers/Cardify.swift
@@ -0,0 +1,50 @@
+//
+//  Cardify.swift
+//  Memorize
+//
+//  Created by Максим Митрофанов on 22.05.2023.
+//
+
+import SwiftUI
+
+struct Cardify: ViewModifier {
+    
+    //Params
+    var isFaceUp: Bool
+    
+    var borderWidth: CGFloat = 3
+    var fillColor: Color = .orange
+    var cornerRadiusOffset: CGFloat = 11
+    var fontOffset: CGFloat = 2
+    
+    func body(content: Content) -> some View {
+        GeometryReader { geometry in
+            let cardShape = createCardShape(for: geometry)
+            
+            ZStack {
+                if isFaceUp {
+                    cardShape.foregroundColor(.white)
+                    cardShape.strokeBorder(lineWidth: borderWidth)
+                    content
+                }
+
+                else { cardShape }
+            }
+            .foregroundColor(fillColor)
+        }
+    }
+    
+    private func createCardShape(for proxy: GeometryProxy) -> RoundedRectangle {
+        RoundedRectangle(cornerRadius: calculateCornerRadius(for: proxy))
+    }
+        
+    private func calculateCornerRadius(for proxy: GeometryProxy) ->  CGFloat {
+        proxy.size.height / cornerRadiusOffset
+   }
+}
+
+extension View {
+    func cardify(isFaceUp: Bool) -> some View {
+        self.modifier(Cardify(isFaceUp: isFaceUp))
+    }
+}
--- Memorize/Memorize/Views/CardView.swift
+++ Memorize/Memorize/Views/CardView.swift
@@ -9,52 +9,32 @@ import SwiftUI
 
 struct CardView: View {
     
-    var card: EmojiMemoryGame.Card
     
     //Params
-    var borderWidth: CGFloat = 4
+    var card: EmojiMemoryGame.Card
     var fillColor: Color = .orange
-    var cornerRadiusOffset: CGFloat = 11
     var fontOffset: CGFloat = 2
     
     //Constants
     private let backgroundCircleScale: CGFloat = 0.85
     
     var body: some View {
-        GeometryReader { geometry in
-            let cardShape = createCardShape(for: geometry)
-            
-            ZStack {
-                if card.isFaceUp {
-                    cardShape.foregroundColor(.white)
-                    cardShape.strokeBorder(lineWidth: borderWidth)
-                    cardContents.font(font(for: geometry))
-                }
-
-                else { cardShape }
-            }
-            .foregroundColor(fillColor)
-        }
+        cardContents.cardify(isFaceUp: card.isFaceUp)
     }
     
     var cardContents: some View {
+        GeometryReader { proxy in
         ZStack {
-            PieShape(
-                startAngle: Angle(degrees: 0 - 90),
-                endAngle: Angle(degrees: 50 - 90))
+                PieShape(
+                    startAngle: Angle(degrees: 0 - 90),
+                    endAngle: Angle(degrees: 50 - 90))
                 .scale(backgroundCircleScale)
                 .opacity(0.55)
-            Text(card.content)
+                Text(card.content)
+                    .font(font(for: proxy))
+            }
         }
     }
-    
-    private func createCardShape(for proxy: GeometryProxy) -> RoundedRectangle {
-        RoundedRectangle(cornerRadius: calculateCornerRadius(for: proxy))
-    }
-        
-    private func calculateCornerRadius(for proxy: GeometryProxy) ->  CGFloat {
-        proxy.size.height / cornerRadiusOffset
-   }
     
     private func font(for geometry: GeometryProxy) -> Font {
         return Font.system(size: geometry.size.width / fontOffset)
