3c3a7424ff1a1b723f7066e3fdc42e01fc95b28e
Maksim Mitrofanov <keyseymiroff@gmail.com>
2023-05-24 12:03:17 +0400

    Added transitions to cards

--- Memorize/Memorize/Views/MemoryGameView.swift
+++ Memorize/Memorize/Views/MemoryGameView.swift
@@ -8,8 +8,8 @@
 import SwiftUI
 
 struct MemoryGameView: View {
+    @State var dealt = Set<Int>()
     @ObservedObject var emojiGame: EmojiMemoryGame
-    private let columns = [GridItem(.adaptive(minimum: 80))]
     
     var body: some View {
         VStack {
@@ -28,7 +28,7 @@ struct MemoryGameView: View {
     
     var cards: some View {
         AspectVGrid(items: emojiGame.cards, aspectRatio: 5/6.5) { card in
-            if card.isMatched && !card.isFaceUp {
+            if isUndealt(card: card) || card.isMatched && !card.isFaceUp {
                 Color.clear
             }
             else {
@@ -39,6 +39,12 @@ struct MemoryGameView: View {
                             emojiGame.choose(card: card)
                         }
                     }
+            }
+        }
+        .onAppear {
+            // "deal" the cards
+            withAnimation {
+                emojiGame.cards.forEach { deal(card: $0) }
             }
         }
     }
@@ -56,8 +62,22 @@ struct MemoryGameView: View {
     }
 }
 
+extension MemoryGameView {
+    // Computed Variables
+    private var columns: [GridItem] { [GridItem(.adaptive(minimum: 80))] }
+    
+    // Functions
+    private func deal(card: EmojiMemoryGame.Card) {
+        dealt.insert(card.id)
+    }
+    
+    private func isUndealt(card: EmojiMemoryGame.Card) -> Bool {
+        !dealt.contains(card.id)
+    }
+}
+
 struct MemoryGameView_Previews: PreviewProvider {
-    static let game = EmojiMemoryGame(cardCount: 26)
+    static let game = EmojiMemoryGame(cardCount: 10)
     
     static var previews: some View {
         game.choose(card: game.cards[0])
