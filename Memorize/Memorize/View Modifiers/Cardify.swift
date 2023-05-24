83659e38cb8c13bdd5eb192ac8c6c017a74bdcbf
Maksim Mitrofanov <keyseymiroff@gmail.com>
2023-05-24 11:22:18 +0400

    Added 3d rotation to Cards

--- Memorize/Memorize/View Modifiers/Cardify.swift
+++ Memorize/Memorize/View Modifiers/Cardify.swift
@@ -7,23 +7,25 @@
 
 import SwiftUI
 
-struct Cardify: ViewModifier {
+struct Cardify: Animatable, ViewModifier {
     
-    //Params
-    var isFaceUp: Bool
+    init(isFaceUp: Bool) {
+        rotation = isFaceUp ? 0 : 180
+    }
     
-    var borderWidth: CGFloat = 3
-    var fillColor: Color = .orange
-    var cornerRadiusOffset: CGFloat = 11
-    var fontOffset: CGFloat = 2
+    var rotation: Double // how much the card is rotated
+    
+    var animatableData: Double {
+        get { rotation }
+        set { rotation = newValue }
+    }
     
     func body(content: Content) -> some View {
         GeometryReader { geometry in
             let cardShape = createCardShape(for: geometry)
             
             ZStack {
-                
-                if isFaceUp {
+                if shouldDisplayFaceUp {
                     cardShape.foregroundColor(.white)
                     cardShape.strokeBorder(lineWidth: borderWidth)
                 }
@@ -32,9 +34,11 @@ struct Cardify: ViewModifier {
                 }
                 
                 content
-                    .opacity(isFaceUp ? 1 : 0)
+                    .opacity(shouldDisplayFaceUp ? 1 : 0)
             }
+            .animation(.none)
             .foregroundColor(fillColor)
+            .rotation3DEffect(Angle(degrees: rotation), axis: (x: 0, y: 1, z: 0))
         }
     }
     
@@ -51,4 +55,13 @@ extension View {
     func cardify(isFaceUp: Bool) -> some View {
         self.modifier(Cardify(isFaceUp: isFaceUp))
     }
+}
+
+extension Cardify {
+    var borderWidth: CGFloat { 3 }
+    var fillColor: Color { .orange }
+    var cornerRadiusOffset: CGFloat { 11 }
+    var fontOffset: CGFloat { 2 }
+    
+    var shouldDisplayFaceUp: Bool { rotation < 90 }
 }
